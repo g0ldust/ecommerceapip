89fe321020918ff2e1190e7cfe8a30950688eda7
g0ldust <164261133+g0ldust@users.noreply.github.com>
2026-05-29 21:31:53 -0400

    strings

--- src/main/java/com/pcfactory/ecommerce/dto/Meal.java
+++ src/main/java/com/pcfactory/ecommerce/dto/Meal.java
@@ -3,5 +3,15 @@ import lombok.Data;
 
 public class Meal {
     private String idMeal;
-    private
+    private String strMeal;
+    private String strMealAlternate;
+    private String strCategory;
+    private String strArea;
+    private String strCountry;
+    private String strInstructions;
+    private String strMealThumb;
+    private String strTags;
+    private String strYoutube;
+
+
 }
