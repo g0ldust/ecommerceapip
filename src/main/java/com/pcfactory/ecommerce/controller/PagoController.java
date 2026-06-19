1ca54164de1a4437e36060efd2789ac88c65c2e3
g0ldust <164261133+g0ldust@users.noreply.github.com>
2026-06-19 12:27:15 -0400

    Update PagoController.java

--- src/main/java/com/pcfactory/ecommerce/controller/PagoController.java
+++ src/main/java/com/pcfactory/ecommerce/controller/PagoController.java
@@ -10,7 +10,7 @@ import reactor.core.publisher.Mono;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/pagos")
+
 
 public class PagoController {
     private final WebpayService webpayService;
